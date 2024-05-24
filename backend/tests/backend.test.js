bae98f813594066409e76f2fd7354a89ac663151
RavenTheII <ravinaaphaasadej@gmail.com>
2024-05-24 18:16:12 +1000

    Fix Jest for backend testing 9

--- backend/tests/backend.test.js
+++ backend/tests/backend.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { print } = require('../server');  // Import the print function
+const { print } = require('../server.js');  // Import the print function
 
 assert.strictEqual(typeof print, 'function', 'print should be a function');
 print();  // Call the print function
